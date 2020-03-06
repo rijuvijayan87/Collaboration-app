b36a18bcf80b7dc6d5e1073f04078c90d1c2cf45
Riju <rijuvijayan@gmail.com>
2020-03-07 12:39:57 +1300

    user registration completed till mongodb

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcyrpt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+
+//adding user models into the code
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user
@@ -17,12 +22,46 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+    try {
+      // Check user exists
+      let user = await User.findOne({ email: email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt password
+      const salt = await bcyrpt.genSalt(10);
+      user.password = await bcyrpt.hash(password, salt);
+      await user.save();
+
+      // Return jsonwebtoken (when the users i want them to be logged in right away and jwt is needed for that workflow)
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
