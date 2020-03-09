6862b5636cffec41111f23794714191b80626818
rijuvijayan <rijuvijayan@gmail.com>
2020-03-09 21:31:46 +1300

    add posts completed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,52 +1,47 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-const PostSchema = new Schema({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String
-  },
-  avatar: {
-    type: String
-  },
-  likes: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      }
-    }
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+
+// @route   POST api/posts
+// @desc    Create a Post
+// @access  Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
   ],
-  comments: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      },
-      text: {
-        type: String,
-        required: true
-      },
-      name: {
-        type: String
-      },
-      avatar: {
-        type: String
-      },
-      date: {
-        type: Date,
-        default: Date.now
-      }
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() });
     }
-  ]
-});
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
 
-module.exports = Post = mongoose.model('post', PostSchema);
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+module.exports = router;
